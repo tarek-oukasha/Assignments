902a9ef94106156b6f2a9b1b0799c357fef792c0
Tarek Oukacha <tarekoukasha2009@gmail.com>
2024-12-15 12:28:06 +0400

    assignment1

---
+++ assignment1.py
@@ -0,0 +1,2 @@
+print("my favorite hobbies are playing football, going swimming and doing mma",end="")
+print("my favorite holday desitantion would have to be visiting georgia in the inter because","it looks so beautiful and cozy wwithe all the snow around it",sep=",           ")
