1ae4a80072c745868a6f08a0c6d334b204cac3fc
Tarek Oukacha <tarekoukasha2009@gmail.com>
2025-01-05 12:29:25 +0400

    added type() function

--- assignment1.py
+++ assignment1.py
@@ -1,2 +1,11 @@
 print("my favorite hobbies are playing football, going swimming and doing mma",end="")
 print("my favorite holday desitantion would have to be visiting georgia in the inter because","it looks so beautiful and cozy wwithe all the snow around it",sep=",           ")
+
+
+a = "my favorite hobbies are playing football, going swimming and doing mma"
+b = 15
+c = ("a","b","c") 
+
+print(type(a))
+print(type(b))
+print(type(c))
