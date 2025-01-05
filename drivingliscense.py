d94976225f102e68d2c03a4e3dace3f1b6a12160
Tarek Oukacha <tarekoukasha2009@gmail.com>
2025-01-05 12:44:28 +0400

    ask user to put in two numbers and the code has to determine wich one is larger

---
+++ drivingliscense.py
@@ -0,0 +1,8 @@
+name = input("what is your name: ")
+age = input("how old are you: ")
+school_name = input("what is the name of your school: ")
+
+print(name)
+print(age)
+print(school_name)
+
---
+++ drivingliscense2.py
@@ -0,0 +1,9 @@
+num1 = input("pick a number: ")
+num2 = input("pick another numbre: ")
+
+if num1>num2 :
+    print(num1,"is bigger than",num2)
+elif num1 == num2 :
+    print(num1," is equal to ",num2)
+else :
+    print(num1,"smaller than",num2)
